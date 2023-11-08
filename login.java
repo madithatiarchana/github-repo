2fd29313ae659d2d5c3582c214a3c19527bee785
archana <archana@gmail.com>
2023-11-08 02:18:36 +0000

    the logic added

--- login.java
+++ login.java
@@ -1,5 +1,7 @@
 // this is the functionality of java file //
 { 
   void main()
-     s.o.p (" enter the name "
+     s.o.p (" enter the user name ")
+      Read the user name
+      s.o.p ( " enter the passwd)
             }
